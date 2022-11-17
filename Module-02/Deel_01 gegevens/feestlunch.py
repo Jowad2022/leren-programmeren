b4c80551a48a104edf06a535af6ec5396e840981
Jowad2022 <118281902+Jowad2022@users.noreply.github.com>
2022-11-17 11:28:28 +0100

    Commit alles

---
+++ Module-02/Deel_01 gegevens/feestlunch.py
@@ -0,0 +1,8 @@
+croissant = 17
+stokbrood = 2
+kortingbon = 3
+
+prijs = (croissant * 0.39 + stokbrood * 2.78) - 3 * 0.50
+
+print("De feestlunch met de kortingsbonnen komt uit op: " , prijs)
+
---
+++ Module-02/Deel_01 gegevens/speelhal.py
@@ -0,0 +1,6 @@
+tijd = 0.37 
+personen = 4
+
+prijs = personen * 7.45 + tijd * (45/5)
+
+print("De prijs voor de speelhal tickets en de VIP vr komt uit op: ", prijs)
