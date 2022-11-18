c5158811411c92c1f15b1877d2bfdc758581d70c
Jowad2022 <118281902+Jowad2022@users.noreply.github.com>
2022-11-18 12:06:33 +0100

    else-statement

---
+++ Module-02/Deel-03 conditions en forks/else-statement.py
@@ -0,0 +1,14 @@
+
+a = 10
+b = 5
+if a > b:
+    max = a
+    min = b
+    print(f"a is het grootste getal:{max}")
+elif a < b:
+    max = b
+    min = a
+    print(f"a is het kleinste getal:{min}")
+else:
+    print("a en b zijn even groot")
+
