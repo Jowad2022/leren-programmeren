e7fab151f2517dbf9c89dbb432d84bb8265270f3
Jowad2022 <118281902+Jowad2022@users.noreply.github.com>
2022-11-18 12:05:01 +0100

    elif-statement

---
+++ Module-02/Deel-03 conditions en forks/elif-statement.py
@@ -0,0 +1,11 @@
+#Jowad al-fartousy
+a = 10
+b = 15
+if a > b:
+    max = a
+    min = b
+    print(f"a is het grootste getal:{max}")
+elif a < b:
+    max = b
+    min = a
+    print(f"a is het kleinste getal:{min}")
