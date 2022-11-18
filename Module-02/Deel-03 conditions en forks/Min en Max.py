02aa1b7ebd7589d557925f13a3ff9270920d4791
Jowad2022 <118281902+Jowad2022@users.noreply.github.com>
2022-11-18 12:08:55 +0100

    Min en Max

---
+++ Module-02/Deel-03 conditions en forks/Min en Max.py
@@ -0,0 +1,16 @@
+
+a = 10
+b = 5
+if a > b:
+    max = a
+    min = b
+    print(f"a is het grootste getal:{max}")
+elif a < b:
+    max = b
+    min = a
+    print(f"a is het kleinste getal:{min}")
+else:
+    print("a en b zijn even groot")
+
+print(f"Het minimum is:{min}")
+print(f"Het maximum is:{max}")
