b4aaf9157a9adc4bcea1a27c3826fb5a3f07b55f
Jowad2022 <99059303@mydavinci.nl>
2023-02-23 10:06:26 +0100

    commit

---
+++ Random opdrachten/zookeeper/Zookeeper.js
@@ -0,0 +1,9 @@
+function bereken_poten(){
+    poten = 0
+    berekening = (poten += vragen)
+    console.log(poten)
+}
+// stoppen = true
+// while (stoppen == true)
+vraag = number(prompt("hoeveel poten zie je ?: "))
+alert(bereken_poten)
