d1695065d11ce1b117fbcbc643f68580057f4f8d
Jowad2022 <99059303@mydavinci.nl>
2023-05-26 10:17:59 +0200

    commit

--- module-05/deel_02/treasure_hunt/functions.py
+++ module-05/deel_02/treasure_hunt/functions.py
@@ -1,6 +1,11 @@
 import time
+import math
 from termcolor import colored
 from data import JOURNEY_IN_DAYS
+from data import COST_FOOD_HUMAN_COPPER_PER_DAY
+from data import COST_FOOD_HORSE_COPPER_PER_DAY 
+from data import COST_TENT_GOLD_PER_WEEK 
+from data import COST_HORSE_SILVER_PER_DAY
 
 ##################### M04.D02.O2 #####################
 
@@ -21,45 +26,84 @@ def platinum2gold(amount:int) -> float:
     return float(platinum)
 
 def getPersonCashInGold(personCash:dict) -> float:
-    pass
+    platinum = personCash ["platinum"] * 25
+    copper = personCash ["copper"] / 50
+    silver = personCash ["silver"] / 5
+    goud = personCash ["gold"] 
+    totaal = platinum + copper + silver + goud
+    return totaal
 
 ##################### M04.D02.O4 #####################
 
 def getJourneyFoodCostsInGold(people:int, horses:int) -> float:
-    pass
+    people_kosten =  people * COST_FOOD_HUMAN_COPPER_PER_DAY * JOURNEY_IN_DAYS
+    horses_kosten =  horses * COST_FOOD_HORSE_COPPER_PER_DAY * JOURNEY_IN_DAYS
+    totaal = people_kosten + horses_kosten
+    return copper2gold(totaal)
 
 ##################### M04.D02.O5 #####################
 
 def getFromListByKeyIs(list:list, key:str, value:any) -> list:
-    pass
-
+    lijst = []
+    for d in list:
+        if key in d and d[key] == value:
+            lijst.append(d)
+    return lijst    
+            
 def getAdventuringPeople(people:list) -> list:
-    pass
+    return getFromListByKeyIs(people,"adventuring",True)
 
 def getShareWithFriends(friends:list) -> int:
-    pass
+    return getFromListByKeyIs(friends,"shareWith",True)
 
 def getAdventuringFriends(friends:list) -> list:
-    pass
+    new_list = []
+    for dict in getAdventuringPeople(friends):
+        if dict in getShareWithFriends(friends):
+            new_list.append(dict)
+    return new_list
 
 ##################### M04.D02.O6 #####################
 
 def getNumberOfHorsesNeeded(people:int) -> int:
-    pass
+    return math.ceil(people / 2)
+
 
 def getNumberOfTentsNeeded(people:int) -> int:
-    pass
+    return math.ceil(people / 3)
 
 def getTotalRentalCost(horses:int, tents:int) -> float:
-    pass
+    horses_s = horses * silver2gold(COST_HORSE_SILVER_PER_DAY) * JOURNEY_IN_DAYS
+    tents_s = tents * COST_TENT_GOLD_PER_WEEK * math.ceil(JOURNEY_IN_DAYS / 7 )
+    totaal = horses_s + tents_s
+    return float(totaal)
 
 ##################### M04.D02.O7 #####################
 
 def getItemsAsText(items:list) -> str:
-    pass
+    text = []
+    for i in items:
+        text.append (f"{i['amount']}{i['unit']} {i['name']}")
+    return ", ".join(text) 
 
-def getItemsValueInGold(items:list) -> float:
-    pass
+def getItemsValueInGold(items: list) -> float:
+    items_value = []
+    for j in items:
+            if j['price']['type'] == "copper":
+                type_copper = j['price']['amount'] * j["amount"]
+                type_gold = copper2gold(type_copper)
+                items_value.append(type_gold)
+            elif j['price']['type'] == "silver":
+                type_silver = j['price']['amount'] * j["amount"]
+                type_gold = silver2gold(type_silver)
+                items_value.append(type_gold)
+            elif j['price']['type'] == "platinum":
+                type_platinum = j['price']['amount'] * j["amount"]
+                type_gold = platinum2gold(type_platinum)
+                items_value.append(type_gold)
+            else:
+                items_value.append(j['price']['amount'] * j["amount"])
+    return sum(items_value)
 
 ##################### M04.D02.O8 #####################
 
